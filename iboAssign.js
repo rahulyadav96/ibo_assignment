48b18f01e198e3ad71cd62a6b38de8311aa44191
rahulyadav96 <rahul.yadav96@hotmail.com>
2022-06-29 12:44:05 +0530

    ibo assignment started

---
+++ iboAssign.js
@@ -0,0 +1,101 @@
+/*
+You have been given a list of products which is having property productName, quantity and description.
+
+
+PROBLEM STATEMENTS:
+
+1. you need to write a function say, getUniqueProductCount which should return count of each Product(as an object) present in the given list of Products considering Product Name as Key.
+
+Sample Output for the given listOfProducts will be :
+
+{
+  "TV": 2,
+  "AC": 2,
+  "FAN": 1
+}
+
+
+
+2. you need to write a function say, getUniquePrducts which should return an array of objects by grouping the products based on the productName and summing up the quantity for the same products present in the given list of Products considering Product Name as Key.
+
+Sample Output for the given listOfProducts will be :
+
+[{
+    productName: "TV",
+    quantity: 20,
+    description: "television"
+  },
+  {
+    productName: "AC",
+    quantity: 10,
+    description: "air conditioner"
+  },
+  {
+    productName: "FAN",
+    quantity: 10,
+     description: "Ceiling Fan"
+  }
+]
+
+*/
+
+const listOfProducts = [
+  {
+    productName: "TV",
+    quantity: 10,
+    description: "television",
+  },
+  {
+    productName: "AC",
+    quantity: 5,
+    description: "air conditioner",
+  },
+  {
+    productName: "TV",
+    quantity: 10,
+    description: "television",
+  },
+  {
+    productName: "AC",
+    quantity: 5,
+    description: "air conditioner",
+  },
+  {
+    productName: "FAN",
+    quantity: 10,
+    description: "Ceiling Fan",
+  },
+];
+
+function getUniqueProductCount(prodList) {
+  let uniqueProdCount = {};
+
+  prodList.forEach((prod) => {
+    let prodName = prod.productName;
+
+    if (uniqueProdCount.hasOwnProperty(prodName)) {
+      uniqueProdCount[prodName] += prod.quantity;
+    } else {
+      uniqueProdCount[prodName] = prod.quantity;
+    }
+  });
+
+  return uniqueProdCount;
+}
+
+function getUniquePrducts(prodList) {
+
+    let uniqueProds = {};
+    prodList.forEach((prod) => {
+        
+    
+        if (uniqueProds.includes(prod)) {
+          uniqueProds[prodName] += prod.quantity;
+        } else {
+          uniqueProds[prodName] = prod.quantity;
+        }
+      });
+    
+      return uniqueProds;
+
+}
